7c960890c61d683693644a95870738641dead379
Tahuana <tahuana@gmail.com>
2018-03-11 20:47:46 -0400

    creating a header component that read the message from state, changing style

--- tahuana-cirati-redux/my-app-redux/src/App.js
+++ tahuana-cirati-redux/my-app-redux/src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 import updateHeaderAction from './actions/header-action';
 
 class App extends Component {
   
-  
+  componentDidMount() {
+    this.props.updateHeader('Gotta Fetch em all!');
+  }
+
   render() {
-    console.log(this.props)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
