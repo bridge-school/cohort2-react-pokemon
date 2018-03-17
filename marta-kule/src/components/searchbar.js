9e893b0bffee9def76a42c579bff7db31f03c2a3
Marta Kule <marta.kule@gmail.com>
2018-03-17 11:50:59 -0400

    Start over

--- marta-kule/src/components/searchbar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const SearchBar = ({onInputChange, inputValue, placeholder}) => {
-	return(
-			<input className="searchBar" onChange={onInputChange} value={inputValue} placeholder={placeholder}/>
-	);
-}
